4488cbaf72bf3ab902d0a09496a272fc92420ff4
Ricardo Tenorio <ricardobatenorio@gmail.com>
2021-12-31 15:26:16 -0300

    Adicionando regra de negócio às entidades

--- src/ModelagemDeDominiosRicos.Vendas.Domain/PedidoItem.cs
+++ src/ModelagemDeDominiosRicos.Vendas.Domain/PedidoItem.cs
@@ -1,12 +1,54 @@
 ﻿using ModelagemDeDominiosRicos.Core.DomainObjects;
+using System;
 
 namespace ModelagemDeDominiosRicos.Vendas.Domain
 {
     public class PedidoItem : Entity
     {
+        public Guid PedidoId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        // EF Rel.
+        public Pedido Pedido { get; set; }
+
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+
+        protected PedidoItem() { }
+
+        internal void AssociarPedido(Guid pedidoId)
+        {
+            PedidoId = pedidoId;
+        }
+
+        public decimal CalcularValor()
+        {
+            return Quantidade * ValorUnitario;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            //if (unidades < 0) unidades *= -1;
+
+            Quantidade += unidades;
+        }
+
+        internal void AtualizarUnidades(int unidades)
+        {
+            Quantidade = unidades;
+        }
+
         public override void Validar()
         {
-            throw new System.NotImplementedException();
+            Validacoes.ValidarSeVazio(ProdutoNome, "Nome do produto é obrigatório.");
         }
     }
 }
